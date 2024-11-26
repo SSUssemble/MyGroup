263ea66ea5e089aa523f76036265ee44f73dd6a7
douglaslee <nbyeon@Outlook.com>
2024-11-26 17:54:20 +0900

    Dark mode fix

--- app/src/main/java/com/example/ssussemble/DarkModeSetting.java
+++ app/src/main/java/com/example/ssussemble/DarkModeSetting.java
@@ -3,57 +3,54 @@ package com.example.ssussemble;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.ssussemble.databinding.ActivityDarkModeSettingBinding;
 
 public class DarkModeSetting extends AppCompatActivity {
     private SharedPreferences sharedPreferences;
-    private Boolean isDarkmode;
-
     private ActivityDarkModeSettingBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityDarkModeSettingBinding.inflate(getLayoutInflater());
-
         setContentView(binding.getRoot());
 
         sharedPreferences = getSharedPreferences("app_settings", MODE_PRIVATE);
-
         SharedPreferences.Editor editor = sharedPreferences.edit();
+
 
         int themeMode = sharedPreferences.getInt("themeMode", AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
 
-        if(themeMode == AppCompatDelegate.MODE_NIGHT_NO){
+
+        if (themeMode == AppCompatDelegate.MODE_NIGHT_NO) {
             binding.radioOFF.setChecked(true);
-        } else if(themeMode ==AppCompatDelegate.MODE_NIGHT_YES){
+        } else if (themeMode == AppCompatDelegate.MODE_NIGHT_YES) {
             binding.radioON.setChecked(true);
-        } else{
+        } else {
             binding.radioAuto.setChecked(true);
         }
 
-        AppCompatDelegate.setDefaultNightMode(themeMode);
 
-        binding.radioGroup.setOnCheckedChangeListener(((radioGroup, i) -> {
-            int selectMode;
-            if(i == R.id.radioON){
-                selectMode = AppCompatDelegate.MODE_NIGHT_NO;
+        binding.radioGroup.setOnCheckedChangeListener((radioGroup, i) -> {
+            int selectedMode;
+
+            if (i == R.id.radioON) {
+                selectedMode = AppCompatDelegate.MODE_NIGHT_YES; 
             } else if (i == R.id.radioOFF) {
-                selectMode = AppCompatDelegate.MODE_NIGHT_YES;
+                selectedMode = AppCompatDelegate.MODE_NIGHT_NO;
             } else {
-                selectMode = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
+                selectedMode = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
             }
 
-            editor.putInt("themeMode", selectMode)
-                    .apply();
-            AppCompatDelegate.setDefaultNightMode(selectMode);
-        }));
+
+            if (selectedMode != themeMode) {
+                editor.putInt("themeMode", selectedMode).apply();
+                AppCompatDelegate.setDefaultNightMode(selectedMode);
+            }
+        });
     }
 }
